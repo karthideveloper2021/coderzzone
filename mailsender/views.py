54c4506b2ce631c719d6a48d56bdb15a0f23af42
karthi <karthi2002smk@gmail.com>
2022-09-05 23:30:18 +0530

    attachment testing

--- mailsender/views.py
+++ mailsender/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from mailsender.models import User
 from mailsender.threading import SendMail
+from mailsender.models import History
+from django.core.mail import EmailMessage
+from coderz.settings import EMAIL_HOST_USER
 import uuid
 
 # Create your views here.
@@ -16,8 +19,29 @@ def home(request):
         
         if user_code.exists():
             trans_id=uuid.uuid4()
-            SendMail(args=(data,files,trans_id)).start()
-            context['success_message']="Mail has been sent to {} successfully with Transaction id: {}".format(data['to-email'],trans_id)
+            # SendMail(args=(data,files,trans_id)).start()
+            user=User.objects.get(code=data['secret-code'])         
+            history=History(uid=user.uid,transaction_id=trans_id)
+            history.save()
+
+            email=EmailMessage(
+                    from_email=EMAIL_HOST_USER,
+                    to=[data['to-email']],
+                    subject=data['subject'],
+                    body=data['body'],
+                )
+            for doc in files:
+                email.attach(doc.name,doc.read(),doc.content_type)
+
+            try:
+                email.send()
+                history.status=True
+                history.save()
+            
+            except:
+                email.to=[user.email]
+                email.send()
+                context['success_message']="Mail has been sent to {} successfully with Transaction id: {}".format(data['to-email'],trans_id)
         else:
             context['error_message']="Sorry, your are not authorized to use this service"
     
