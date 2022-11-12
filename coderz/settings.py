295985426ebe15e585bf2bc2d385dd537ca40066
karthi <karthi2002smk@gmail.com>
2022-11-12 23:04:57 +0530

    final

--- coderz/settings.py
+++ coderz/settings.py
@@ -24,14 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY=config('SECRET_KEY')
+SECRET_KEY=config('SECRET_KEY',cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['https://coderzzone.herokuapp.com','localhost']
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
